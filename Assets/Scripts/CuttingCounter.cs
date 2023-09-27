0b988f27e1c7f13a7a5356a06da0ad8421dfe3d6
John Perkins <jcperkins482@gmail.com>
2023-09-26 22:05:57 -0400

    Added Recipes for CuttingSO

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-	[SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+	[SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
 	public override void Interact(Player player)
 	{
 		if (!HasKitchenObject())
@@ -12,7 +12,12 @@ public class CuttingCounter : BaseCounter
 			// There is no kitchen object here	
 			if (player.HasKitchenObject())
 			{
-				player.GetKitchenObject().SetKitchenObjectParent(this);
+				if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+				{
+					// Player carrying something that can be cut
+					player.GetKitchenObject().SetKitchenObjectParent(this);
+				}
+				
 			}
 		}
 		else
@@ -31,11 +36,37 @@ public class CuttingCounter : BaseCounter
 
 	public override void InteractAlternate(Player player)
 	{
-		if (HasKitchenObject())
+		if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
 		{
 			// There is a kitchen object here
+			KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
 			GetKitchenObject().DestroySelf();
-			KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+			KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
 		}
+	}
+
+	private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+	{
+		foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+		{
+			if (cuttingRecipeSO.input == inputKitchenObjectSO)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+	{
+		foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+		{
+			if (cuttingRecipeSO.input == inputKitchenObjectSO)
+			{
+				return cuttingRecipeSO.output;
+			}
+		}
+
+		return null;
 	}
 }
