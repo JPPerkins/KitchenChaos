165835338da8bc89092cd76a7576e304565db2f1
John Perkins <jcperkins482@gmail.com>
2023-09-26 21:26:45 -0400

    Added Extra Kitchen Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,26 @@ public class ClearCounter : BaseCounter
 
 	public override void Interact(Player player)
 	{
+		if (!HasKitchenObject())
+		{
+			// There is no kitchen object here	
+			if (player.HasKitchenObject())
+			{
+				player.GetKitchenObject().SetKitchenObjectParent(this);
+			}
+		}
+		else
+		{
+			//there is an kitchen object here
+			if (player.HasKitchenObject())
+			{
+				
+			}
+			else
+			{
+				GetKitchenObject().SetKitchenObjectParent(player);
+			}
+		}
 		
 	}
 }
