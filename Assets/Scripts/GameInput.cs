2a1baaf313805a6701db6ed033bbbf2600f009e6
John Perkins <jcperkins482@gmail.com>
2023-09-28 00:30:21 -0400

    Key rebindings

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,13 +6,24 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+	private const string PLAYER_PREFS_BINDINGS = "InputBindings";
+	
 	public static GameInput Instance { get; private set; }
 	
 	public event EventHandler OnInteractAction;
 	public event EventHandler OnInteractAlternateAction;
 	public event EventHandler OnPauseAction;
 
-	
+	public enum Binding
+	{
+		Move_Up,
+		Move_Down,
+		Move_Left,
+		Move_Right,
+		Interact,
+		Interact_Alternate,
+		Pause
+	}
     private PlayerInputActions playerInputActions;
 	
 	private void Awake()
@@ -20,11 +31,19 @@ public class GameInput : MonoBehaviour
 		Instance = this;
 		
 		playerInputActions = new PlayerInputActions();
+		
+		if (PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS))
+		{
+			playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+		}
+		
 		playerInputActions.Player.Enable();
 
 		playerInputActions.Player.Interact.performed += Interact_Performed;
 		playerInputActions.Player.InteractAlternate.performed += InteractAlternate_Performed;
 		playerInputActions.Player.Pause.performed += Pause_Performed;
+
+
 	}
 
 	private void OnDestroy()
@@ -58,5 +77,83 @@ public class GameInput : MonoBehaviour
 		inputVector = inputVector.normalized;
 
 		return inputVector;
+	}
+
+	public string GetBindingText(Binding binding)
+	{
+		switch (binding)
+		{
+			case Binding.Move_Up:
+				return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+			case Binding.Move_Down:
+				return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+			case Binding.Move_Left:
+				return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+			case Binding.Move_Right:
+				return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+			case Binding.Interact:
+				return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+			case Binding.Interact_Alternate:
+				return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+			case Binding.Pause:
+				return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+			default:
+				throw new ArgumentOutOfRangeException(nameof(binding), binding, null);
+		}
+	}
+
+	public void RebindBinding(Binding binding, Action onActionRebound)
+	{
+		playerInputActions.Player.Disable();
+
+		InputAction inputAction;
+		int bindingIndex;
+		
+		switch (binding)
+		{
+			case Binding.Move_Up:
+				inputAction = playerInputActions.Player.Move;
+				bindingIndex = 1;
+				break;
+			case Binding.Move_Down:
+				inputAction = playerInputActions.Player.Move;
+				bindingIndex = 2;
+				break;
+			case Binding.Move_Left:
+				inputAction = playerInputActions.Player.Move;
+				bindingIndex = 3;
+				break;
+			case Binding.Move_Right:
+				inputAction = playerInputActions.Player.Move;
+				bindingIndex = 4;
+				break;
+			case Binding.Interact:
+				inputAction = playerInputActions.Player.Interact;
+				bindingIndex = 0;
+				break;
+			case Binding.Interact_Alternate:
+				inputAction = playerInputActions.Player.InteractAlternate;
+				bindingIndex = 0;
+				break;
+			case Binding.Pause:
+				inputAction = playerInputActions.Player.Pause;
+				bindingIndex = 0;
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(binding), binding, null);
+		}
+		
+		inputAction.PerformInteractiveRebinding(bindingIndex)
+			.OnComplete(callback => 
+			{
+				callback.Dispose();
+				playerInputActions.Player.Enable();
+				onActionRebound();
+
+				PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
+				PlayerPrefs.Save();
+			})
+			.Start();
+		
 	}
 }
